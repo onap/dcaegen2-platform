77900bb3097491cd9fca964c111ea70724e53989
Michael Hwang <mhwang@research.att.com>
2019-11-11 15:28:57 -0500

    Add distributor api project
    Issue-ID: DCAEGEN2-1860
    Signed-off-by: Michael Hwang <mhwang@research.att.com>
    Change-Id: I67aa9178b1b1830e330ca1259e8f6b30202945df

---
+++ mod/distributorapi/distributor/config.py
@@ -0,0 +1,38 @@
+# ============LICENSE_START=======================================================
+# Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+"""Configuration for distributor api"""
+import os, tempfile, six, inspect
+from datetime import datetime
+from distributor import errors
+
+
+def _grab_env(name, default=None):
+    try:
+        if default:
+            return os.environ.get(name, default)
+        else:
+            return os.environ[name]
+    except KeyError:
+        raise errors.DistributorAPIConfigError("Required environment variable missing: {0}".format(name))
+
+def init():
+    global nifi_registry_url
+    nifi_registry_url = _grab_env("NIFI_REGISTRY_URL"
+            , default="http://nifi-registry:18080/nifi-registry-api")
+
+    global onboarding_api_url
+    onboarding_api_url = _grab_env("ONBOARDING_API_URL"
+            , default="http://onboarding-api/onboarding")
---
+++ mod/distributorapi/distributor/utils.py
@@ -0,0 +1,43 @@
+# ============LICENSE_START=======================================================
+# Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+"""Utility functions"""
+
+from urllib.parse import quote
+import requests as reqs
+from distributor import errors
+
+
+def urljoin(base, *trailing, **query_params):
+    base = base[0:-1] if base[-1] == "/" else base
+    full = [base] + list(trailing)
+    url = "/".join(full)
+
+    if query_params:
+        qp = ["{0}={1}".format(quote(k), quote(str(v))) for k,v in query_params.items()]
+        qp = "&".join(qp)
+        return "?".join([url, qp])
+    else:
+        return url
+
+
+def get_json(url):
+    resp = reqs.get(url)
+
+    try:
+        resp.raise_for_status()
+        return resp.json()
+    except Exception as e:
+        raise errors.DistributorAPIError(e)
