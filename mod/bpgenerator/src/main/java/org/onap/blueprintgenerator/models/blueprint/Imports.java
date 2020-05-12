8479ac77ba084f9d42e16b252e6671ff59aedd13
PANCHAL <dp023n@att.com>
2020-05-12 09:12:14 -0400

    Change R4 plugins to R6
    Change bpgen R4 references to R6 because Runtime APi is defaulting to R4
    
    Issue-ID: DCAEGEN2-2197
    Signed-off-by: PANCHAL <dp023n@att.com>
    Change-Id: Id98fee8b5125cc056d852c343f3cc1d2df7e0a5d

--- mod/bpgenerator/src/main/java/org/onap/blueprintgenerator/models/blueprint/Imports.java
+++ mod/bpgenerator/src/main/java/org/onap/blueprintgenerator/models/blueprint/Imports.java
@@ -48,8 +48,8 @@ public class Imports {
 	public static ArrayList<String> createOnapImports() {
 		ArrayList<String> imps = new ArrayList<String>();
 		imps.add("http://www.getcloudify.org/spec/cloudify/3.4/types.yaml");
-		imps.add("https://nexus.onap.org/service/local/repositories/raw/content/org.onap.dcaegen2.platform.plugins/R4/k8splugin/1.4.5/k8splugin_types.yaml");
-		imps.add("https://nexus.onap.org/service/local/repositories/raw/content/org.onap.dcaegen2.platform.plugins/R4/dcaepolicyplugin/2.3.0/dcaepolicyplugin_types.yaml");
+		imps.add("https://nexus.onap.org/service/local/repositories/raw/content/org.onap.dcaegen2.platform.plugins/R6/k8splugin/1.7.2/k8splugin_types.yaml");
+		imps.add("https://nexus.onap.org/service/local/repositories/raw/content/org.onap.dcaegen2.platform.plugins/R6/dcaepolicyplugin/2.4.0/dcaepolicyplugin_types.yaml");
 		return imps;
 	}
 	public static ArrayList<String> createDmaapImports(){
