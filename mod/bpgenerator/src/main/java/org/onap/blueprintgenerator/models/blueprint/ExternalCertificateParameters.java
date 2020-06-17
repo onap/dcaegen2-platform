f3ea1e8021f81153dac8779e902f4058e883f105
Remigiusz Janeczek <remigiusz.janeczek@nokia.com>
2020-06-17 17:52:56 +0200

    Add external tls info to bpgenerator and component spec schema
    Issue-ID: DCAEGEN2-2251
    Signed-off-by: Remigiusz Janeczek <remigiusz.janeczek@nokia.com>
    Change-Id: I2f976ccc3e0b271bf9ae1357f02bd86fe0903459

--- mod/bpgenerator/src/main/java/org/onap/blueprintgenerator/models/blueprint/ExternalCertificateParameters.java
+++ mod/bpgenerator/src/main/java/org/onap/blueprintgenerator/models/blueprint/ExternalCertificateParameters.java
@@ -1,15 +1,13 @@
 /**============LICENSE_START=======================================================
  org.onap.dcae
  ================================================================================
- Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
- ================================================================================
- Modifications Copyright (c) 2020 Nokia. All rights reserved.
+ Copyright (c) 2020 Nokia. All rights reserved.
  ================================================================================
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
 
-      http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,18 +17,22 @@
  ============LICENSE_END=========================================================
 
  */
-
 package org.onap.blueprintgenerator.models.blueprint;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
-public class GetInput {
+public class ExternalCertificateParameters {
 
-	private Object get_input;
+	@JsonProperty("common_name")
+	private GetInput commonName;
+
+	@JsonProperty("sans")
+	private GetInput sans;
+
 }
--- mod/bpgenerator/src/main/java/org/onap/blueprintgenerator/models/blueprint/ExternalTlsInfo.java
+++ mod/bpgenerator/src/main/java/org/onap/blueprintgenerator/models/blueprint/ExternalTlsInfo.java
@@ -1,15 +1,13 @@
 /**============LICENSE_START=======================================================
  org.onap.dcae
  ================================================================================
- Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
- ================================================================================
- Modifications Copyright (c) 2020 Nokia. All rights reserved.
+ Copyright (c) 2020 Nokia. All rights reserved.
  ================================================================================
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
 
-      http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,18 +17,27 @@
  ============LICENSE_END=========================================================
 
  */
-
 package org.onap.blueprintgenerator.models.blueprint;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
-public class GetInput {
+public class ExternalTlsInfo {
 
-	private Object get_input;
+	@JsonProperty("external_cert_directory")
+	private String externalCertDirectory;
+
+	@JsonProperty("use_external_tls")
+	private GetInput useExternalTls;
+
+	@JsonProperty("ca_name")
+	private GetInput caName;
+
+	@JsonProperty("external_certificate_parameters")
+	private ExternalCertificateParameters externalCertificateParameters;
 }
