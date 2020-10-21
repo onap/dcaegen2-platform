42a845e2fdfb09c0d1bc4152e155b1dc62e35299
Ravi Mantena <rx908f@att.com>
2020-10-21 15:47:38 -0400

    Blueprint Generator Refactored Code Issue-ID: DCAEGEN2-2472
    Change-Id: I2ade7fb3b4196554eb4ecadd918fd45cb6ba8a52
    Signed-off-by: Ravi Mantena <rx908f@att.com>

---
+++ mod/bpgenerator/common/src/main/java/org/onap/blueprintgenerator/service/base/BlueprintService.java
@@ -0,0 +1,159 @@
+/*
+ *
+ *  * ============LICENSE_START=======================================================
+ *  *  org.onap.dcae
+ *  *  ================================================================================
+ *  *  Copyright (c) 2020  AT&T Intellectual Property. All rights reserved.
+ *  *  ================================================================================
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *  *  ============LICENSE_END=========================================================
+ *
+ *
+ */
+
+package org.onap.blueprintgenerator.service.base;
+
+import org.onap.blueprintgenerator.model.base.Blueprint;
+import org.onap.blueprintgenerator.model.common.Input;
+import org.onap.blueprintgenerator.model.componentspec.base.ComponentSpec;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * @author : Ravi Mantena
+ * @date 10/16/2020
+ * Application: DCAE/ONAP - Blueprint Generator
+ * Common Module: Used by both ONAp and DCAE Blueprint Applications
+ * Service: For Adding Quotes and Converting Blueprint to Yaml/String
+ */
+
+@Service
+public abstract class BlueprintService {
+
+    @Autowired
+    protected ImportsService importsService;
+
+    @Autowired
+    protected FixesService fixesService;
+
+    @Qualifier("objectMapper")
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Qualifier("yamlObjectMapper")
+    @Autowired
+    protected ObjectMapper yamlObjectMapper;
+
+    public void blueprintToYaml(ComponentSpec cs, Blueprint blueprint, Input input) {
+        String bluePrintName = input.getBluePrintName();
+        String outputPath = input.getOutputPath();
+        String comment = "# " + input.getComment() + '\n';
+
+        try {
+            File outputFile;
+            String name = bluePrintName.equals("") ? cs.getSelf().getName() : bluePrintName;
+            if(name.contains(".")) {
+                name = name.replaceAll(Pattern.quote("."), "_");
+            }
+            if(name.contains(" ")) {
+                name = name.replaceAll(" ", "");
+            }
+            String file = name + ".yaml";
+            outputFile = new File(outputPath, file);
+            outputFile.getParentFile().mkdirs();
+            try {
+                outputFile.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            String version = "#blueprint_version: " + cs.getSelf().getVersion() + '\n';
+            String description = "#description: " + cs.getSelf().getDescription() + '\n';
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile, false));
+            writer.write(description);
+            writer.write(version);
+
+            if(input.getBpType().equals("dti") || input.getBpType().equals("m") || input.getBpType().equals("k"))
+                writer.write(comment);
+
+            writer.close();
+
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(outputFile, true)));
+            yamlObjectMapper.writeValue(out, blueprint);
+            out.close();
+
+            if(input.getBpType().equals("dti") || input.getBpType().equals("m") || input.getBpType().equals("k"))
+                fixesService.fixDcaeSingleQuotes(outputFile);
+            else
+                fixesService.fixOnapSingleQuotes(outputFile);
+
+            //new Yaml().load(new FileInputStream(outputFile));
+
+            System.out.println("Blueprint is created with valid YAML Format");
+        } catch (Exception ex) {
+            throw new RuntimeException("Unable to generate YAML file from Blueprint  or the generated YAML is not valid", ex);
+        }
+    }
+
+    // Used for DCAE
+    public Map<String, LinkedHashMap<String, Object>> addQuotes(Map<String, LinkedHashMap<String, Object>> inputs) {
+
+        inputs.forEach((key,value) -> {
+            if(value.get("type") != null){
+                if (value.get("type").equals("string") && value.get("default") != null && !key.contains("policies")){
+                    value.replace("default",  "'" + value.get("default").toString() + "'");
+                } else if (value.get("type").equals("map") || value.get("type").equals("list")) {
+                    // Commented the Code as we need to read the object as is for Map and List. If the List object is to be converted to string uncomment the below code.
+                    /* if(inputs.get(s).get("type").equals("list")) {
+                        String temp = inputs.get(s).get("default").toString();
+                        inputs.get(s).replace("default", temp);
+                    }*/
+                    inputs.get(key).remove("type");
+                }
+            }
+        });
+
+         return inputs;
+    }
+
+    public String blueprintToString(ComponentSpec componentSpec, Blueprint blueprint, Input input) {
+        String ret = "";
+        try {
+            ret = yamlObjectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(blueprint);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        if(input.getBpType().equals("dti") || input.getBpType().equals("m") || input.getBpType().equals("k")){
+            ret = "# " + componentSpec.getSelf().getDescription() + "\n" + "# " + componentSpec.getSelf().getVersion() + "\n" + "# " + input.getComment() + "\n" + ret;
+            ret = fixesService.fixStringQuotes(ret);
+        }else
+            ret = fixesService.applyFixes(ret);
+        return ret;
+    }
+
+
+}
---
+++ mod/bpgenerator/common/src/main/java/org/onap/blueprintgenerator/service/base/ImportsService.java
@@ -0,0 +1,189 @@
+/*
+ *
+ *  * ============LICENSE_START=======================================================
+ *  *  org.onap.dcae
+ *  *  ================================================================================
+ *  *  Copyright (c) 2020  AT&T Intellectual Property. All rights reserved.
+ *  *  ================================================================================
+ *  *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  *  You may obtain a copy of the License at
+ *  *
+ *  *       http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  *  Unless required by applicable law or agreed to in writing, software
+ *  *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  See the License for the specific language governing permissions and
+ *  *  limitations under the License.
+ *  *  ============LICENSE_END=========================================================
+ *
+ *
+ */
+
+package org.onap.blueprintgenerator.service.base;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.onap.blueprintgenerator.model.common.Imports;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : Ravi Mantena
+ * @date 10/16/2020
+ * Application: DCAE/ONAP - Blueprint Generator
+ * Common Module: Used by both ONAp and DCAE Blueprint Applications
+ * Service: For Imports
+ */
+
+@Service
+public class ImportsService {
+
+    @Value("${imports.docker.types}")
+    private String importsDockerTypes;
+
+    @Value("${imports.docker.dockerplugin.node.type}")
+    private String importsDockerDockerpluginNodetype;
+
+    @Value("${imports.docker.dmaap}")
+    private String importsDockerDmaap;
+
+    @Value("${imports.docker.relationship.types}")
+    private String importsDockerRelationshipTypes;
+
+    @Value("${imports.docker.dcaepolicyplugin.node.type}")
+    private String importsDockerDcaepolicypluginNodeType;
+
+    @Value("${imports.docker.pgaas.types}")
+    private String importsDockerPgaasTypes;
+
+    @Value("${imports.K8s.types}")
+    private String importsK8sTypes;
+
+    @Value("${imports.K8s.node.type}")
+    private String importsK8sNodetype;
+
+    @Value("${imports.K8s.relationship.types}")
+    private String importsK8sRelationshipTypes;
+
+    @Value("${imports.K8s.dmaapplugin.node.type}")
+    private String importsK8sDmaapPluginNodeType;
+
+    @Value("${imports.K8s.dcaepolicyplugin.node.type}")
+    private String importsK8sDcaepolicypluginNodeType;
+
+    @Value("${imports.K8s.pgaas.types}")
+    private String importsK8sPgaasTypes;
+
+    @Value("${imports.eom.docker.types}")
+    private String importsEOMDockerTypes;
+
+    @Value("${imports.eom.docker.dockerplugin.node.type}")
+    private String importsEOMDockerDockerpluginNodetype;
+
+    @Value("${imports.eom.docker.dmaapplugin.node.type}")
+    private String importsEOMDockerDmaappluginNodetype;
+
+    @Value("${imports.eom.docker.relationship.types}")
+    private String importsEOMDockerRelationshipTypes;
+
+    @Value("${imports.eom.docker.dcaepolicyplugin.node.type}")
+    private String importsEOMDockerDcaepolicypluginNodeType;
+
+    @Value("${imports.eom.docker.pgaas.types}")
+    private String importsEOMDockerPgaasTypes;
+
+    @Value("${imports.onap.types}")
+    private String importsOnapTypes;
+
+    @Value("${imports.onap.K8s.plugintypes}")
+    private String importsOnapK8sPlugintypes;
+
+    @Value("${imports.onap.K8s.dcaepolicyplugin}")
+    private String importsOnapK8sDcaepolicyplugin;
+
+    @Value("${imports.dmaap.types}")
+    private String importsDmaapTypes;
+
+    @Value("${imports.dmaap.K8s.plugintypes}")
+    private String importsDmaapK8sPlugintypes;
+
+    @Value("${imports.dmaap.dmaapplugin}")
+    private String importsDmaapDmaapplugin;
+
+    @Value("${import.Cloudify}")
+    private String importCloudify;
+
+    @Value("${import.Postgres}")
+    private String importPostgres;
+
+    @Value("${import.Clamp}")
+    private String importClamp;
+
+    @Value("${mrTopic.URL}")
+    private String mrTopicURL;
+
+    @Qualifier("yamlObjectMapper")
+    @Autowired
+    protected ObjectMapper yamlObjectMapper;
+
+    public List<String> createImports(String bpType) {
+        List<String> imports = new ArrayList<>();
+        if (bpType.equals("dti")) {
+            imports.add(importsDockerTypes);
+            imports.add(importsDockerDockerpluginNodetype);
+            imports.add(importsDockerDmaap);
+            imports.add(importsDockerRelationshipTypes);
+            imports.add(importsDockerDcaepolicypluginNodeType);
+            imports.add(importsDockerPgaasTypes);
+        } else if (bpType.equals("k")) {
+            imports.add(importsK8sTypes);
+            imports.add(importsK8sNodetype);
+            imports.add(importsK8sRelationshipTypes);
+            imports.add(importsK8sDmaapPluginNodeType);
+            imports.add(importsK8sDcaepolicypluginNodeType);
+            imports.add(importsK8sPgaasTypes);
+        } else if (bpType.equals("m")) {
+            imports.add(importsEOMDockerTypes);
+            imports.add(importsEOMDockerDockerpluginNodetype);
+            imports.add(importsEOMDockerDmaappluginNodetype);
+            imports.add(importsEOMDockerRelationshipTypes);
+            imports.add(importsEOMDockerDcaepolicypluginNodeType);
+            imports.add(importsEOMDockerPgaasTypes);
+        }
+        else if (bpType.equals("o")) {
+            imports.add(importsOnapTypes);
+            imports.add(importsOnapK8sPlugintypes);
+            imports.add(importsOnapK8sDcaepolicyplugin);
+            imports.add(importCloudify);
+            imports.add(importPostgres);
+            imports.add(importClamp);
+            imports.add(mrTopicURL);
+        }
+        else if (bpType.equals("d")) {
+            imports.add(importsDmaapTypes);
+            imports.add(importsDmaapK8sPlugintypes);
+            imports.add(importsDmaapDmaapplugin);
+            imports.add(importCloudify);
+            imports.add(importPostgres);
+            imports.add(importClamp);
+            imports.add(mrTopicURL);
+        }
+        return imports;
+    }
+
+    public List<String> createImportsFromFile(String path) throws IOException {
+        File importPath = new File(path);
+        Imports imports = yamlObjectMapper.readValue(importPath, Imports.class);
+        imports.getImports().removeIf(String::isBlank);
+        return imports.getImports();
+    }
+
+}
