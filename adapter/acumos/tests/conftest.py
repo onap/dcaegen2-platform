5326197b0545f6c786a2aadfe28b9d56d42d5c9d
shabs2020 <shabnam.sultana@highstreet-technologies.com>
2021-12-06 09:35:52 +0100

    Acumos Adapter updates to support v3 schema
    Enhanced Adapter to support component spec v3 schema of Istanbul release
    
    Additionally corrected config file for no_proxy variable
    
    Issue-ID: DCAEGEN2-2877
    Signed-off-by: shabs2020 <shabnam.sultana@highstreet-technologies.com>
    Change-Id: Ie6f2decb6afcd1e164fafab6fd943d6b6c8e7cf0
    Signed-off-by: shabs2020 <shabnam.sultana@highstreet-technologies.com>

--- adapter/acumos/tests/conftest.py
+++ adapter/acumos/tests/conftest.py
@@ -2,6 +2,7 @@
 # org.onap.dcae
 # =============================================================================
 # Copyright (c) 2020 AT&T Intellectual Property. All rights reserved.
+# Copyright (c) 2021 highstreet technologies GmbH. All rights reserved.
 # =============================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,7 +29,7 @@ from tests import testing_helpers
 def mock_schemas(monkeypatch):
     cwd = os.getcwd()
     schemadir = cwd[:cwd.find('/adapter/acumos')] + '/mod/component-json-schemas'
-    monkeypatch.setattr(aoconversion.utils.component_schema, 'path', schemadir + '/component-specification/dcae-cli-v2/component-spec-schema.json')
+    monkeypatch.setattr(aoconversion.utils.component_schema, 'path', schemadir + '/component-specification/dcae-cli-v3/component-spec-schema.json')
     monkeypatch.setattr(aoconversion.utils.dataformat_schema, 'path', schemadir + '/data-format/dcae-cli-v1/data-format-schema.json')
     monkeypatch.setattr(aoconversion.utils.schema_schema, 'ret', requests.get('https://json-schema.org/draft-04/schema#').json())
 
