edcc322ad1d353cb5bfa48b0d6a74d041f3e9a32
Hansen, Tony (th1395) <th1395@att.com>
2022-08-05 22:16:14 +0000

    codecoverage improvement
    Change-Id: I69cdb741c1e07a096ddd7c85921326070fb08a92
    Signed-off-by: Hansen, Tony (th1395) <th1395@att.com>
    Issue-ID: DCAEGEN2-3170
    Signed-off-by: Hansen, Tony (th1395) <th1395@att.com>

--- mod/distributorapi/setup.py
+++ mod/distributorapi/setup.py
@@ -1,5 +1,5 @@
 # ============LICENSE_START=======================================================
-# Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
+# Copyright (c) 2019-2022 AT&T Intellectual Property. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -38,6 +38,7 @@ setup(
             "flask-restplus"
             , "Flask-Cors"
             , "requests"
+	    , "MarkupSafe==2.0.1"
             ],
         zip_safe = False
         )
