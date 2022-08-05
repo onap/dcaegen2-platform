d6fe6071d7f64ab4888748fb9d52314fad6c125f
Hansen, Tony (th1395) <th1395@att.com>
2022-08-05 22:16:14 +0000

    codecoverage improvement
    Change-Id: I69cdb741c1e07a096ddd7c85921326070fb08a92
    Signed-off-by: Hansen, Tony (th1395) <th1395@att.com>
    Issue-ID: DCAEGEN2-3170

--- mod/distributorapi/distributor/version.py
+++ mod/distributorapi/distributor/version.py
@@ -1,5 +1,5 @@
 # ============LICENSE_START=======================================================
-# Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
+# Copyright (c) 2019-2022 AT&T Intellectual Property. All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============LICENSE_END=========================================================
-__version__ = "1.0.1"
+__version__ = "1.1.0"
